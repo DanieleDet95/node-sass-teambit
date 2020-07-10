e432a046e71b58fc44949af92a209f2aad1d4e0f
Daniele <danieledetommaso1995@gmail.com>
2020-07-10 17:19:06 +0200

    Completamento footer

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready( function (){
 
+  // Al click visualizzare o nascondere il sotto menu 
   $('li.sotto').click( function(){
     $(this).find('.sotto_menu').toggleClass('nascosto');
   });
